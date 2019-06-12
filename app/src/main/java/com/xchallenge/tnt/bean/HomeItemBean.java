b6e38bb0655553b7b4ef1b47a4301b0307c3accc
WangEric-95 <785332394@qq.com>
2019-06-12 12:40:48 +0800

    home fragment change

---
+++ app/src/main/java/com/xchallenge/tnt/bean/HomeItemBean.java
@@ -0,0 +1,12 @@
+package com.xchallenge.tnt.bean;
+
+import java.util.ArrayList;
+
+public class HomeItemBean {
+    public String itemName;
+    public String[] items;
+    public HomeItemBean(String itemName,String[] items) {
+        this.itemName=itemName;
+        this.items=items;
+    }
+}
